f4bdc2394cb5f03cb4a96679890aed36931ff72d
shuoscaryo <shuoscaryo@gmail.com>
2023-11-03 15:33:15 +0100

    [t_keys]<2023-11-03-15:32> Modified main.c and movement.c: This changes are to start using t_keys struct

--- src/main.c
+++ src/main.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   main.c                                             :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: iortega- <iortega-@student.42madrid.com    +#+  +:+       +#+        */
+/*   By: orudek <orudek@student.42madrid.com>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/11/01 12:14:02 by iortega-          #+#    #+#             */
-/*   Updated: 2023/11/02 21:09:59 by iortega-         ###   ########.fr       */
+/*   Updated: 2023/11/03 14:29:49 by orudek           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,13 +16,16 @@ int	main(int argc, char **argv)
 {
 	t_game	game;
 
-	if (!check_params(argc, argv, &game))
-		return (0);
-	if (!read_map(&game.map))
-		return (free_map(&game.map), 0);
+	//if (!check_params(argc, argv, &game))
+	//	return (0);
+	//game_init(&game);
 	game.mlx = mlx_init();
-	game.win = mlx_new_window(game.mlx, 1080, 720, "cub3d");
-	mlx_key_hook(game.win, select_move, &game);
+	game.win = mlx_new_window(game.mlx, WIN_WIDTH, WIN_HEIGHT, "cub3d");
+	//mlx_hook(game.win, ON_DESTROY, NO_EVENT_MASK, ft_game_exit, &game);
+	mlx_hook(game.win, ON_KEYDOWN, KEY_PRESS_MASK, on_key_down, &game);
+	mlx_hook(game.win, ON_KEYUP, KEY_RELEASE_MASK, on_key_up, &game);
+	mlx_do_key_autorepeatoff(game.mlx);
+	mlx_do_sync(game.mlx);
 	mlx_loop(game.mlx);
 	return (0);
 }
--- src/movement.c
+++ src/movement.c
@@ -3,17 +3,42 @@
 /*                                                        :::      ::::::::   */
 /*   movement.c                                         :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: iortega- <iortega-@student.42madrid.com    +#+  +:+       +#+        */
+/*   By: orudek <orudek@student.42madrid.com>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/11/01 13:32:03 by iortega-          #+#    #+#             */
-/*   Updated: 2023/11/02 18:46:32 by iortega-         ###   ########.fr       */
+/*   Updated: 2023/11/03 14:18:27 by orudek           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "cub3d.h"
 
-int	select_move(int key, t_game *game)
+int on_key_up(int key, t_game *game)
 {
+	printf("called on_key_up :");
+	if (key == ESC)
+	{
+		mlx_destroy_window(game->mlx, game->win);
+		game->win = NULL;
+		free(game->map.SO);
+		free(game->map.NO);
+		free(game->map.WE);
+		free(game->map.EA);
+		exit(0);
+	}
+	else if (key == W)
+		printf("W\n");
+	else if (key == A)
+		printf("A\n");
+	else if (key == S)
+		printf("S\n");
+	else if (key == D)
+		printf("D\n");
+	return (0);
+}
+
+int	on_key_down(int key, t_game *game)
+{
+	printf("called on_key_down: ");
 	if (key == ESC)
 	{
 		mlx_destroy_window(game->mlx, game->win);

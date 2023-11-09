a3601f374cdf092b971cc0da61347e43224907e3
shuoscaryo <shuoscaryo@gmail.com>
2023-11-09 18:12:34 +0100

    [dev]<2023-11-09-18:12> Fixed ray iteration: Each ray casted should move to the next grid now and return the tile where it landed

--- src/render/render.c
+++ src/render/render.c
@@ -11,34 +11,47 @@ typedef struct s_ray
 	float	delta_z;
 }	t_ray;
 
-/*static float	move_next_point(t_ray *ray, char **map)
+static float	move_next_point(t_ray *ray, char **map, int *new_x, int *new_y)
 {
 	float	tx;
 	float	ty;
 
 	*new_x = (int)ray->x + (ray->delta_x > 0 ) - (ray->delta_x < 0 );
 		tx = (*new_x - ray->x) / ray->delta_x; //calculate time to right grid
-	*new_y = (int)ray->y + (ray->delta_y > 0 );
+	*new_y = (int)ray->y + (ray->delta_y > 0 ) - (ray->delta_y < 0 );
 		ty = (*new_y - ray->y) / ray->delta_y; //calculate time to up grid
 	if (tx > ty) // if time to right grid is bigger than time to up grid
 	{
+		new_x = (int)ray->x;
 		tx = ty;
-		//if (is_wall(map, (int)ray->x, (int)ray->y))
-			//return (tx);
 	}
 	else
-	{
-		if (is_wall(map, (int)ray->x, (int)ray->y))
-		{
-			printf("MURO\n");
-			printf("x: %f, y: %f\n", ray->x, ray->y);
-			return (tx);
-		}
-	}
+		new_y = (int)ray->y;
 	ray->x += ray->delta_x * tx;
 	ray->y += ray->delta_y * tx;
 	ray->z += ray->delta_z * tx;
-	map++;
+	return (tx);
+}
+
+static float	move_first_point(t_ray *ray, char **map, int *new_x, int *new_y)
+{
+	float	tx;
+	float	ty;
+
+	*new_x = (int)ray->x + (ray->delta_x > 0 );
+		tx = (*new_x - ray->x) / ray->delta_x; //Check if delta_x is 0
+	*new_y = (int)ray->y + (ray->delta_y > 0 );
+		ty = (*new_y - ray->y) / ray->delta_y; //Check if delta_y is 0
+	if (tx > ty) // if time to right grid is bigger than time to up grid
+	{
+		new_x = (int)ray->x;
+		tx = ty;
+	}
+	else
+		new_y = (int)ray->y;
+	ray->x += ray->delta_x * tx;
+	ray->y += ray->delta_y * tx;
+	ray->z += ray->delta_z * tx;
 	return (tx);
 }
 
@@ -47,13 +60,21 @@ static int	get_pixel(t_game *game, t_ray *ray)
 	int		t;
 
 	t = 0;
-	t += move_next_point(ray, game->map.map);
+	t += move_first_point(ray, game->map.map);
+	if (ray->z > 1)
+		return (game->map.F[0] << 16 | game->map.F[1] << 8 | game->map.F[2]);
+	if (ray->z < 0)
+		return (game->map.C[0] << 16 | game->map.C[1] << 8 | game->map.C[2]);
+	if (is_wall())
+		return (Color of wall);
 	while (t < RENDER_DISTANCE)
 	{
 		if (ray->z > 1)
 			return (game->map.F[0] << 16 | game->map.F[1] << 8 | game->map.F[2]);
 		if (ray->z < 0)
 			return (game->map.C[0] << 16 | game->map.C[1] << 8 | game->map.C[2]);
+		if (is_wall())
+			return (Color of wall);
 		t += move_next_point(ray, game->map.map);
 	}
 	if (ray->z < game->player.z)
@@ -77,17 +98,16 @@ t_img *render(t_game *game , t_img *img)
 		y = -1;
 		while (++y < img->height)
 		{
-			alpha = game->player.rotation + tan((img->width - x) / frame_dist);
-			beta = tan((img->height - y) / frame_dist);
+			alpha = game->player.rotation + tan((img->width - x) / frame_dist); //NOTE UPDATE WITH NEW COORDINATES
+			beta = tan((img->height - y) / frame_dist); //NOTE UPDATE WITH NEW COORDINATES
 			ray.x = game->player.x;
 			ray.y = game->player.y;
 			ray.z = game->player.z;
-			ray.delta_x = cos(beta) * cos(alpha);
-			ray.delta_y = cos(beta) * sin(alpha);
-			ray.delta_z = sin(beta);
+			ray.delta_x = cos(beta) * cos(alpha); //NOTE UPDATE WITH NEW COORDINATES
+			ray.delta_y = cos(beta) * sin(alpha); //NOTE UPDATE WITH NEW COORDINATES
+			ray.delta_z = sin(beta); //NOTE UPDATE WITH NEW COORDINATES
 			img->put_pixel(img, x, y, get_pixel(game, &ray));
 		}
 	}
 	return (img);
 }
-*/

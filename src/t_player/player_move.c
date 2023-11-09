d41c724806a7cf73db2c6df06940fce29b7458d1
shuoscaryo <shuoscaryo@gmail.com>
2023-11-09 12:52:43 +0100

    [dev]<2023-11-09-12:52> Optimized player move when hitting a wall: Removed some useless calculations

--- src/t_player/player_move.c
+++ src/t_player/player_move.c
@@ -6,7 +6,7 @@
 /*   By: orudek <orudek@student.42madrid.com>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/11/03 21:44:16 by orudek            #+#    #+#             */
-/*   Updated: 2023/11/09 12:28:03 by orudek           ###   ########.fr       */
+/*   Updated: 2023/11/09 12:51:27 by orudek           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -59,11 +59,11 @@ void	player_move(t_player *this, t_game *game, float delta_time)
 	this->rotation += this->v_rotation * delta_time;
 	adjust_rotation(&this->rotation);
 	if (is_wall(game->map.map, (int)(x + this->size * ((vx > 0) - (vx < 0))), y))
-		this->x = (int)(x + this->size * ((vx > 0) - (vx < 0))) + (vx < 0) + this->size * ((vx < 0) - (vx > 0));
+		this->x = (int)(x) + (vx > 0) + this->size * ((vx < 0) - (vx > 0));
 	else
 		this->x = x;
 	if (is_wall(game->map.map, x, (int)(y + this->size * ((vy < 0) - (vy > 0)))))
-		this->y = (int)(y + this->size * ((vy < 0) - (vy > 0))) + (vy > 0) + this->size * ((vy > 0) - (vy < 0));
+		this->y = (int)(y) + (vy < 0) + this->size * ((vy > 0) - (vy < 0));
 	else
 		this->y = y;
 }
